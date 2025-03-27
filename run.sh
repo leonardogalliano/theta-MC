2210eb03bf76d57cd1f01312217d6b745f01b828
Leonardo Galliano <leonardo.galliano@gmail.com>
2025-03-27 14:27:44 +0100

    Add run.sh

---
+++ run.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+
+#
+#$ -cwd
+#$ -j y
+#$ -N theta
+#$ -m ae
+#$ -S /bin/bash
+#$ -V
+#$ -o log_output/output_$JOB_ID
+#
+
+# Positional arguments
+if [[ $# -ge 4 ]]; then
+  init_file="$1"
+  steps="$2"
+  shift 2
+else
+  echo "Error: init_file, steps required."
+  exit 1
+fi
+
+# Optional arguments
+lambda="0.0"
+n="1"
+nsim="1"
+nblocks="1"
+seed="1"
+
+# Read options
+while [[ $# -gt 0 ]]; do
+  case "$1" in
+    --lambda)
+      lambda="$2"
+      shift 2
+      ;;
+    --n)
+      n="$2"
+      shift 2
+      ;;
+    --nsim)
+      nsim="$2"
+      shift 2
+      ;;
+    --nblocks)
+      nblocks="$2"
+      shift 2
+      ;;
+    --seed)
+      seed="$2"
+      shift 2
+      ;;
+    *)
+      shift
+      ;;
+  esac
+done
+
+
+julia --project=. -t $nsim main.jl $init_file $steps --lambda $lambda -n $n --nsim $nsim --nblocks $nblocks --seed $seed
